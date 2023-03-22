4b55f81a7da293f3471aaf59650becc19d77568d
zameer2020 <zameerbashamirza2020@gmail.com>
2023-03-22 14:31:04 +0530

    commit -m "fewfiles ignored"

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
 const heading=document.createElement('h1');
 heading.innerHTML="nAMASTE jAVASCRIPT";
 const root=document.getElementById('root');
 root.appendChild(heading);
+
+
+const head=React.createElement('h1',{},"Namaste React");
+    const head1=React.createElement('h1',{},"Namaste React");
+    const head2=React.createElement('h2',{},"Namaste React");
+    const head3=React.createElement('h3',{},"Namaste React");
+    const content=React.createElement('div',{id:"container",
+    
+    style:{backgroundcolor:"yellow",color:"blue"}},[head,head1,head2,head3])
+
+    const root1=ReactDOM.createRoot(document.getElementById('root'));
+    root1.render(content)
